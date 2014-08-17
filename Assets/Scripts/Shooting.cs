65dcee0482913bfdf6c91a6a610fdf389a26dcee
DarrenBlack <whiteon01@gmail.com>
2014-08-18 00:23:33 +0100

    Added pistol(no new graphics just characteristics), also added ability to make semi automatic weapons

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,6 +6,8 @@ public class Shooting : MonoBehaviour {
     public GameObject bullet;
     public GameObject spentBullet;
 
+    public bool isAutomatic;
+
     public float fireRate = .5f;
     private float nextFire = 0.0f;
     private Vector3 currentPosition;
@@ -28,26 +30,46 @@ public class Shooting : MonoBehaviour {
 	void Update () {
         currentPosition = new Vector3(this.rigidbody2D.position.x,this.rigidbody2D.position.y,0);
 
-        if (Input.GetMouseButton(0) && Time.time > nextFire){
-            nextFire = Time.time + fireRate;         
+        if (Input.GetMouseButton(0)) {
+            if (isAutomatic && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                ShootBullet();
+            }            
+        }
 
-            float randZ = Random.Range(bulletSpreadMin, bulletSpreadMax);
-
-            bullet = Instantiate(bullet, currentPosition, transform.rotation) as GameObject;
-            bullet.transform.Rotate(0, 0, randZ);
-
-            randZ = Random.Range(0, 45);            
-            spentBullet = Instantiate(spentBullet, currentPosition, transform.rotation) as GameObject;
-            spentBullet.transform.Translate(new Vector3(-3f, 0, 0)); //instantiates spent bullet further back          
-
-            float randDrag = Random.Range(minSpentBulletDrag, maxSpentBulletDrag); //Varies distance spent bullet flies from gun
-            randZ = Random.Range(spentBulletSpreadMin, spentBulletSpreadMax);
-            spentBullet.transform.Rotate(0, 0, randZ);
-            Vector3 velocity = spentBullet.transform.rotation * Vector3.down * 10000f;
-            
-            
-            spentBullet.rigidbody2D.AddForce(velocity);
-            spentBullet.rigidbody2D.drag = randDrag; 
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isAutomatic)
+            {
+                ShootBullet();
+            }
         }
 	}
+
+    void ShootBullet()
+    {
+        float randZ = Random.Range(bulletSpreadMin, bulletSpreadMax);
+
+        bullet = Instantiate(bullet, currentPosition, transform.rotation) as GameObject;
+        bullet.transform.Rotate(0, 0, randZ);
+        EjectBullet();
+    }
+
+    void EjectBullet()
+    {       
+        spentBullet = Instantiate(spentBullet, currentPosition, transform.rotation) as GameObject;
+        spentBullet.transform.Translate(new Vector3(-3f, 0, 0)); //instantiates spent bullet further back          
+
+        float randDrag = Random.Range(minSpentBulletDrag, maxSpentBulletDrag); //Varies distance spent bullet flies from gun
+        float randZ = Random.Range(spentBulletSpreadMin, spentBulletSpreadMax);
+        spentBullet.transform.Rotate(0, 0, randZ);
+        Vector3 velocity = spentBullet.transform.rotation * Vector3.down * 10000f;
+
+
+        spentBullet.rigidbody2D.AddForce(velocity);
+        spentBullet.rigidbody2D.drag = randDrag;
+    }
+
+
 }
